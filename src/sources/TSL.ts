ad1497a29ec10de297ca8c6ad6ebd716326435a1
Kevin Sauter <kevin.sauter@adition.com>
2024-08-20 14:04:39 +0200

    Add SimplyLive source using TSL UDP v5

--- src/sources/TSL.ts
+++ src/sources/TSL.ts
@@ -1,4 +1,3 @@
-import {logger} from "..";
 import {RegisterTallyInput} from "../_decorators/RegisterTallyInput.decorator";
 import {FreePort, UsePort} from "../_decorators/UsesPort.decorator";
 import {Source} from '../_models/Source';
@@ -9,44 +8,9 @@ import net from "net";
 import dgram from "dgram";
 import {jspack} from "jspack";
 
-const TALLY_COLOR_ACTION_NONE = 'none'
-const TALLY_COLOR_ACTION_PREVIEW = 'preview'
-const TALLY_COLOR_ACTION_PROGRAM = 'program'
+const TSLFields: TallyInputConfigField[] = [{fieldName: 'port', fieldLabel: 'Port', fieldType: 'port'}];
 
-const TSLFieldsV3: TallyInputConfigField[] = [{fieldName: 'port', fieldLabel: 'Port', fieldType: 'port'}];
-const TSLFieldsV5: TallyInputConfigField[] = [
-    {fieldName: 'port', fieldLabel: 'Port', fieldType: 'port'},
-    {
-        fieldName: 'mode',
-        fieldLabel: 'Tally color mode',
-        fieldType: 'bool',
-        help: 'Per default, `tally_text` is used to indicate if the device is in program or preview (0 => none / 1 => program / 2 => preview / 3 => both). If "Tally color mode" is active, any color on RH or LH indicate an active signal.',
-    },
-    {
-        fieldName: 'tallyLeft',
-        fieldLabel: 'Tally Left',
-        fieldType: 'dropdown',
-        optional: true,
-        help: 'TSL left tally mapping (default: preview)',
-        options: [{id: TALLY_COLOR_ACTION_PROGRAM, label: 'program'}, {
-            id: TALLY_COLOR_ACTION_PREVIEW,
-            label: 'preview'
-        }, {id: TALLY_COLOR_ACTION_NONE, label: 'disabled'}],
-    },
-    {
-        fieldName: 'tallyRight',
-        fieldLabel: 'Tally Right',
-        fieldType: 'dropdown',
-        optional: true,
-        help: 'TSL right tally mapping (default: program)',
-        options: [{id: TALLY_COLOR_ACTION_PROGRAM, label: 'program'}, {
-            id: TALLY_COLOR_ACTION_PREVIEW,
-            label: 'preview'
-        }, {id: TALLY_COLOR_ACTION_NONE, label: 'disabled'}],
-    },
-];
-
-@RegisterTallyInput("5e0a1d8c", "TSL 3.1 UDP", "", TSLFieldsV3)
+@RegisterTallyInput("5e0a1d8c", "TSL 3.1 UDP", "", TSLFields)
 export class TSL3UDPSource extends TallyInput {
     private server: any;
 
@@ -82,7 +46,7 @@ export class TSL3UDPSource extends TallyInput {
     }
 }
 
-@RegisterTallyInput("dc75100e", "TSL 3.1 TCP", "", TSLFieldsV3)
+@RegisterTallyInput("dc75100e", "TSL 3.1 TCP", "", TSLFields)
 export class TSL3TCPSource extends TallyInput {
     private server: any;
 
@@ -170,95 +134,84 @@ export class TSL3TCPSource extends TallyInput {
     }
 }
 
+export class TSL5DataParser {
+    public static parseTSL5Data(data) {
+        let tallyobj: any = {};
+
+        var cursor = 0;
+
+        //Message Format
+        const _PBC = 2 //bytes
+        const _VAR = 1
+        const _FLAGS = 1
+        const _SCREEN = 2
+        const _INDEX = 2
+        const _CONTROL = 2
+
+        //Display Data
+        const _LENGTH = 2
+
+        tallyobj.PBC = jspack.Unpack("<H", data, cursor);
+        cursor += _PBC;
+
+        tallyobj.VAR = jspack.Unpack("<B", data, cursor);
+        cursor += _VAR;
+
+        tallyobj.FLAGS = jspack.Unpack("<B", data, cursor);
+        cursor += _FLAGS;
+
+        tallyobj.SCREEN = jspack.Unpack("<H", data, cursor);
+        cursor += _SCREEN;
+
+        tallyobj.INDEX = jspack.Unpack("<H", data, cursor);
+        cursor += _INDEX;
+
+        tallyobj.CONTROL = jspack.Unpack("<H", data, cursor);
+        cursor += _CONTROL;
+
+        tallyobj.control = {};
+        tallyobj.control.rh_tally = (tallyobj.CONTROL >> 0 & 0b11);
+        tallyobj.control.text_tally = (tallyobj.CONTROL >> 2 & 0b11);
+        tallyobj.control.lh_tally = (tallyobj.CONTROL >> 4 & 0b11);
+        tallyobj.control.brightness = (tallyobj.CONTROL >> 6 & 0b11);
+        tallyobj.control.reserved = (tallyobj.CONTROL >> 8 & 0b1111111);
+        tallyobj.control.control_data = (tallyobj.CONTROL >> 15 & 0b1);
+
+        var LENGTH = jspack.Unpack("<H", data, cursor)
+        cursor += _LENGTH;
+
+        tallyobj.TEXT = jspack.Unpack("s".repeat(LENGTH), data, cursor)
+        return tallyobj;
+    }
+}
+
 class TSL5Base extends TallyInput {
     protected processTSL5Tally(data) {
         if (data.length > 12) {
-
-            let tallyobj: any = {};
-
-            var cursor = 0;
-
-            //Message Format
-            const _PBC = 2 //bytes
-            const _VAR = 1
-            const _FLAGS = 1
-            const _SCREEN = 2
-            const _INDEX = 2
-            const _CONTROL = 2
-
-            //Display Data
-            const _LENGTH = 2
-
-            tallyobj.PBC = jspack.Unpack("<H", data, cursor);
-            cursor += _PBC;
-
-            tallyobj.VAR = jspack.Unpack("<B", data, cursor);
-            cursor += _VAR;
-
-            tallyobj.FLAGS = jspack.Unpack("<B", data, cursor);
-            cursor += _FLAGS;
-
-            tallyobj.SCREEN = jspack.Unpack("<H", data, cursor);
-            cursor += _SCREEN;
-
-            tallyobj.INDEX = jspack.Unpack("<H", data, cursor);
-            cursor += _INDEX;
-
-            tallyobj.CONTROL = jspack.Unpack("<H", data, cursor);
-            cursor += _CONTROL;
-
-            tallyobj.control = {};
-            tallyobj.control.rh_tally = (tallyobj.CONTROL >> 0 & 0b11);
-            tallyobj.control.text_tally = (tallyobj.CONTROL >> 2 & 0b11);
-            tallyobj.control.lh_tally = (tallyobj.CONTROL >> 4 & 0b11);
-            tallyobj.control.brightness = (tallyobj.CONTROL >> 6 & 0b11);
-            tallyobj.control.reserved = (tallyobj.CONTROL >> 8 & 0b1111111);
-            tallyobj.control.control_data = (tallyobj.CONTROL >> 15 & 0b1);
-
-            var LENGTH = jspack.Unpack("<H", data, cursor)
-            cursor += _LENGTH;
-
-            tallyobj.TEXT = jspack.Unpack("s".repeat(LENGTH), data, cursor)
+            let tallyobj: any = TSL5DataParser.parseTSL5Data(data)
 
             this.renameAddress(tallyobj.INDEX[0].toString(), tallyobj.INDEX[0].toString(), tallyobj.TEXT.toString().trim());
 
             let inPreview = 0;
             let inProgram = 0;
-            if (this.source.data.mode === true) {
-                let sourceSettings = this.source.data
-                if (tallyobj.control.rh_tally !== 0) {
-                    if (sourceSettings.tallyRight === TALLY_COLOR_ACTION_PROGRAM || sourceSettings.tallyRight == undefined) {
-                        inProgram = 1
-                    } else if (sourceSettings.tallyRight === TALLY_COLOR_ACTION_PREVIEW) {
-                        inPreview = 1
-                    }
-                }
-                if (tallyobj.control.lh_tally !== 0) {
-                    if (sourceSettings.tallyLeft === TALLY_COLOR_ACTION_PREVIEW || sourceSettings.tallyLeft == undefined) {
-                        inPreview = 1
-                    } else if (sourceSettings.tallyLeft === TALLY_COLOR_ACTION_PROGRAM) {
-                        inProgram = 1
-                    }
-                }
-            } else {
-                switch (tallyobj.control.text_tally) {
-                    case 0:
-                        inPreview = 0;
-                        inProgram = 0;
-                        break;
-                    case 1:
-                        inPreview = 0;
-                        inProgram = 1;
-                        break;
-                    case 2:
-                        inPreview = 1;
-                        inProgram = 0;
-                        break;
-                    case 3:
-                        inPreview = 1;
-                        inProgram = 1;
-                        break;
-                }
+
+            switch (tallyobj.control.text_tally) {
+                case 0:
+                    inPreview = 0;
+                    inProgram = 0;
+                    break;
+                case 1:
+                    inPreview = 0;
+                    inProgram = 1;
+                    break;
+                case 2:
+                    inPreview = 1;
+                    inProgram = 0;
+                    break;
+                case 3:
+                    inPreview = 1;
+                    inProgram = 1;
+                    break;
             }
 
             const busses = [];
@@ -268,15 +221,13 @@ class TSL5Base extends TallyInput {
             if (inProgram) {
                 busses.push("program");
             }
-            logger(tallyobj.TEXT + " state ist jetzt: " + busses, "info")
             this.setBussesForAddress(tallyobj.INDEX[0].toString(), busses);
-            // this.sendIndividualTallyData(tallyobj.INDEX[0].toString(), busses);
-            this.sendTallyData()
+            this.sendIndividualTallyData(tallyobj.INDEX[0].toString(), busses);
         }
     }
 }
 
-@RegisterTallyInput("54237da7", "TSL 5.0 UDP", "", TSLFieldsV5)
+@RegisterTallyInput("54237da7", "TSL 5.0 UDP", "", TSLFields)
 export class TSL5UDPSource extends TSL5Base {
     private server: any;
 
@@ -303,7 +254,7 @@ export class TSL5UDPSource extends TSL5Base {
     }
 }
 
-@RegisterTallyInput("560d3065", "TSL 5.0 TCP", "", TSLFieldsV5)
+@RegisterTallyInput("560d3065", "TSL 5.0 TCP", "", TSLFields)
 export class TSL5TCPSource extends TSL5Base {
     private server: any;
 
