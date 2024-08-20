f39b00e3236079cb6632bfe03c40deea6b1ed91b
Kevin Sauter <kevin.sauter@adition.com>
2024-08-20 09:42:46 +0200

    Add more settings to TSL v5 source

--- src/sources/TSL.ts
+++ src/sources/TSL.ts
@@ -1,19 +1,55 @@
-import { logger } from "..";
-import { RegisterTallyInput } from "../_decorators/RegisterTallyInput.decorator";
-import { FreePort, UsePort } from "../_decorators/UsesPort.decorator";
-import { Source } from '../_models/Source';
-import { TallyInputConfigField } from "../_types/TallyInputConfigField";
-import { TallyInput } from './_Source';
+import {logger} from "..";
+import {RegisterTallyInput} from "../_decorators/RegisterTallyInput.decorator";
+import {FreePort, UsePort} from "../_decorators/UsesPort.decorator";
+import {Source} from '../_models/Source';
+import {TallyInputConfigField} from "../_types/TallyInputConfigField";
+import {TallyInput} from './_Source';
 import TSLUMD from 'tsl-umd';
 import net from "net";
 import dgram from "dgram";
-import { jspack } from "jspack";
+import {jspack} from "jspack";
 
-const TSLFields: TallyInputConfigField[] = [{ fieldName: 'port', fieldLabel: 'Port', fieldType: 'port' }];
+const TALLY_COLOR_ACTION_NONE = 'none'
+const TALLY_COLOR_ACTION_PREVIEW = 'preview'
+const TALLY_COLOR_ACTION_PROGRAM = 'program'
 
-@RegisterTallyInput("5e0a1d8c", "TSL 3.1 UDP", "", TSLFields)
+const TSLFieldsV3: TallyInputConfigField[] = [{fieldName: 'port', fieldLabel: 'Port', fieldType: 'port'}];
+const TSLFieldsV5: TallyInputConfigField[] = [
+    {fieldName: 'port', fieldLabel: 'Port', fieldType: 'port'},
+    {
+        fieldName: 'mode',
+        fieldLabel: 'Tally color mode',
+        fieldType: 'bool',
+        help: 'Per default, `tally_text` is used to indicate if the device is in program or preview (0 => none / 1 => program / 2 => preview / 3 => both). If "Tally color mode" is active, any color on RH or LH indicate an active signal.',
+    },
+    {
+        fieldName: 'tallyLeft',
+        fieldLabel: 'Tally Left',
+        fieldType: 'dropdown',
+        optional: true,
+        help: 'TSL left tally mapping (default: preview)',
+        options: [{id: TALLY_COLOR_ACTION_PROGRAM, label: 'program'}, {
+            id: TALLY_COLOR_ACTION_PREVIEW,
+            label: 'preview'
+        }, {id: TALLY_COLOR_ACTION_NONE, label: 'disabled'}],
+    },
+    {
+        fieldName: 'tallyRight',
+        fieldLabel: 'Tally Right',
+        fieldType: 'dropdown',
+        optional: true,
+        help: 'TSL right tally mapping (default: program)',
+        options: [{id: TALLY_COLOR_ACTION_PROGRAM, label: 'program'}, {
+            id: TALLY_COLOR_ACTION_PREVIEW,
+            label: 'preview'
+        }, {id: TALLY_COLOR_ACTION_NONE, label: 'disabled'}],
+    },
+];
+
+@RegisterTallyInput("5e0a1d8c", "TSL 3.1 UDP", "", TSLFieldsV3)
 export class TSL3UDPSource extends TallyInput {
     private server: any;
+
     constructor(source: Source) {
         super(source);
         this.connected.next(true);
@@ -32,7 +68,7 @@ export class TSL3UDPSource extends TallyInput {
                 busses.push("program");
             }
             this.setBussesForAddress(tally.address, busses);
-			this.sendIndividualTallyData(tally.address, busses);
+            this.sendIndividualTallyData(tally.address, busses);
         });
 
         this.connected.next(true);
@@ -46,9 +82,10 @@ export class TSL3UDPSource extends TallyInput {
     }
 }
 
-@RegisterTallyInput("dc75100e", "TSL 3.1 TCP", "", TSLFields)
+@RegisterTallyInput("dc75100e", "TSL 3.1 TCP", "", TSLFieldsV3)
 export class TSL3TCPSource extends TallyInput {
     private server: any;
+
     constructor(source: Source) {
         super(source);
 
@@ -58,63 +95,62 @@ export class TSL3TCPSource extends TallyInput {
 
         this.server = net.createServer((socket) => {
             socket.on('data', (data) => {
-				//split data up delimiter
+                //split data up delimiter
 
-				var messages = [];
-				var len = data.length;
-				var i = 0;
-				let chunkSize = 18;
+                var messages = [];
+                var len = data.length;
+                var i = 0;
+                let chunkSize = 18;
 
-				while (i < len) {
-					messages.push(data.slice(i, i += chunkSize));
-				}
-				
-				function getBits(byte) {
-					let bits = [];
-					
-					// Loop through each bit position
-					for (let i = 7; i >= 0; i--) {
-						// Shift the bits to the right to isolate the current bit
-						let bit = (byte >> i) & 1;
-						
-						// Add the bit to the array
-						bits.push(bit);
-					}
-					
-					return bits;
-				}
+                while (i < len) {
+                    messages.push(data.slice(i, i += chunkSize));
+                }
 
-				//parse each message
-				for (let buf of messages) {
-					//get the control byte
-					let controlByte = buf.readUInt8(1);
-					let bits = getBits(controlByte);
+                function getBits(byte) {
+                    let bits = [];
 
-					//parse the data
-					let address = buf.readUInt8(0) - 0x80;
-					let brightness = bits[2] + bits[1];
-					let tally4 = bits[4];
-					let tally3 = bits[5];
-					let tally2 = bits[6];
-					let tally1 = bits[7];
+                    // Loop through each bit position
+                    for (let i = 7; i >= 0; i--) {
+                        // Shift the bits to the right to isolate the current bit
+                        let bit = (byte >> i) & 1;
 
-					let label = buf.toString('utf8', 2).trim();
+                        // Add the bit to the array
+                        bits.push(bit);
+                    }
 
-					this.renameAddress(address.toString(), address.toString(), label);
+                    return bits;
+                }
 
-					const busses = [];
-					if (tally1) {
-						busses.push("preview");
-					}
-					if (tally2) {
-						busses.push("program");
-					}
-					//add support here for tally3 and tally4
+                //parse each message
+                for (let buf of messages) {
+                    //get the control byte
+                    let controlByte = buf.readUInt8(1);
+                    let bits = getBits(controlByte);
 
-					this.setBussesForAddress(address.toString(), busses);
-					//this.sendIndividualTallyData(address.toString(), busses);
-					this.sendTallyData();
-				}
+                    //parse the data
+                    let address = buf.readUInt8(0) - 0x80;
+                    let brightness = bits[2] + bits[1];
+                    let tally4 = bits[4];
+                    let tally3 = bits[5];
+                    let tally2 = bits[6];
+                    let tally1 = bits[7];
+
+                    let label = buf.toString('utf8', 2).trim();
+
+                    this.renameAddress(address.toString(), address.toString(), label);
+
+                    const busses = [];
+                    if (tally1) {
+                        busses.push("preview");
+                    }
+                    if (tally2) {
+                        busses.push("program");
+                    }
+                    //add support here for tally3 and tally4
+                    this.setBussesForAddress(address.toString(), busses);
+                    //this.sendIndividualTallyData(address.toString(), busses);
+                    this.sendTallyData();
+                }
             });
 
             socket.on('close', () => {
@@ -127,7 +163,8 @@ export class TSL3TCPSource extends TallyInput {
 
     public exit(): void {
         super.exit();
-        this.server.close(() => { });
+        this.server.close(() => {
+        });
         FreePort(this.source.data.port, this.source.id);
         this.connected.next(false);
     }
@@ -152,22 +189,22 @@ class TSL5Base extends TallyInput {
             //Display Data
             const _LENGTH = 2
 
-            tallyobj.PBC = jspack.Unpack( "<H", data, cursor);
+            tallyobj.PBC = jspack.Unpack("<H", data, cursor);
             cursor += _PBC;
 
-            tallyobj.VAR = jspack.Unpack( "<B", data, cursor);
+            tallyobj.VAR = jspack.Unpack("<B", data, cursor);
             cursor += _VAR;
 
-            tallyobj.FLAGS = jspack.Unpack( "<B", data, cursor);
+            tallyobj.FLAGS = jspack.Unpack("<B", data, cursor);
             cursor += _FLAGS;
 
-            tallyobj.SCREEN = jspack.Unpack( "<H", data, cursor);
+            tallyobj.SCREEN = jspack.Unpack("<H", data, cursor);
             cursor += _SCREEN;
 
-            tallyobj.INDEX = jspack.Unpack( "<H", data, cursor);
+            tallyobj.INDEX = jspack.Unpack("<H", data, cursor);
             cursor += _INDEX;
 
-            tallyobj.CONTROL = jspack.Unpack( "<H", data, cursor);
+            tallyobj.CONTROL = jspack.Unpack("<H", data, cursor);
             cursor += _CONTROL;
 
             tallyobj.control = {};
@@ -178,35 +215,52 @@ class TSL5Base extends TallyInput {
             tallyobj.control.reserved = (tallyobj.CONTROL >> 8 & 0b1111111);
             tallyobj.control.control_data = (tallyobj.CONTROL >> 15 & 0b1);
 
-            var LENGTH = jspack.Unpack( "<H", data, cursor)
+            var LENGTH = jspack.Unpack("<H", data, cursor)
             cursor += _LENGTH;
 
-            tallyobj.TEXT = jspack.Unpack( "s".repeat(LENGTH), data, cursor)
+            tallyobj.TEXT = jspack.Unpack("s".repeat(LENGTH), data, cursor)
 
-			this.renameAddress(tallyobj.INDEX[0].toString(), tallyobj.INDEX[0].toString(), tallyobj.TEXT.toString().trim()); 
+            this.renameAddress(tallyobj.INDEX[0].toString(), tallyobj.INDEX[0].toString(), tallyobj.TEXT.toString().trim());
 
             let inPreview = 0;
             let inProgram = 0;
-            
-            switch(tallyobj.control.text_tally) {
-                case 0:
-                    inPreview = 0;
-                    inProgram = 0;
-                    break;
-                case 1:
-                    inPreview = 0;
-                    inProgram = 1;
-                    break;
-                case 2:
-                    inPreview = 1;
-                    inProgram = 0;
-                    break;
-                case 3:
-                    inPreview = 1;
-                    inProgram = 1;
-                    break;
+            if (this.source.data.mode === true) {
+                let sourceSettings = this.source.data
+                if (tallyobj.control.rh_tally !== 0) {
+                    if (sourceSettings.tallyRight === TALLY_COLOR_ACTION_PROGRAM || sourceSettings.tallyRight == undefined) {
+                        inProgram = 1
+                    } else if (sourceSettings.tallyRight === TALLY_COLOR_ACTION_PREVIEW) {
+                        inPreview = 1
+                    }
+                }
+                if (tallyobj.control.lh_tally !== 0) {
+                    if (sourceSettings.tallyLeft === TALLY_COLOR_ACTION_PREVIEW || sourceSettings.tallyLeft == undefined) {
+                        inPreview = 1
+                    } else if (sourceSettings.tallyLeft === TALLY_COLOR_ACTION_PROGRAM) {
+                        inProgram = 1
+                    }
+                }
+            } else {
+                switch (tallyobj.control.text_tally) {
+                    case 0:
+                        inPreview = 0;
+                        inProgram = 0;
+                        break;
+                    case 1:
+                        inPreview = 0;
+                        inProgram = 1;
+                        break;
+                    case 2:
+                        inPreview = 1;
+                        inProgram = 0;
+                        break;
+                    case 3:
+                        inPreview = 1;
+                        inProgram = 1;
+                        break;
+                }
             }
-            
+
             const busses = [];
             if (inPreview) {
                 busses.push("preview");
@@ -214,15 +268,18 @@ class TSL5Base extends TallyInput {
             if (inProgram) {
                 busses.push("program");
             }
+            logger(tallyobj.TEXT + " state ist jetzt: " + busses, "info")
             this.setBussesForAddress(tallyobj.INDEX[0].toString(), busses);
-            this.sendIndividualTallyData(tallyobj.INDEX[0].toString(), busses);
+            // this.sendIndividualTallyData(tallyobj.INDEX[0].toString(), busses);
+            this.sendTallyData()
         }
     }
 }
 
-@RegisterTallyInput("54237da7", "TSL 5.0 UDP", "", TSLFields)
+@RegisterTallyInput("54237da7", "TSL 5.0 UDP", "", TSLFieldsV5)
 export class TSL5UDPSource extends TSL5Base {
     private server: any;
+
     constructor(source: Source) {
         super(source);
         let port = source.data.port;
@@ -246,9 +303,10 @@ export class TSL5UDPSource extends TSL5Base {
     }
 }
 
-@RegisterTallyInput("560d3065", "TSL 5.0 TCP", "", TSLFields)
+@RegisterTallyInput("560d3065", "TSL 5.0 TCP", "", TSLFieldsV5)
 export class TSL5TCPSource extends TSL5Base {
     private server: any;
+
     constructor(source: Source) {
         super(source);
 
@@ -270,7 +328,8 @@ export class TSL5TCPSource extends TSL5Base {
 
     public exit(): void {
         super.exit();
-        this.server.close(() =>  {});
+        this.server.close(() => {
+        });
         FreePort(this.source.data.port, this.source.id);
         this.connected.next(false);
     }
